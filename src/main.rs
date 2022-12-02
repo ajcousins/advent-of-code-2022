88fa091fd4d8f6c6c50c74dd9a73f32730848e04
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-12-02 19:00:48 +0000

    Day 2.1

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
-use std::fs;
-use std::str::FromStr;
+use std::collections::HashMap;
 
 fn main() {
-
-    let contents = fs::read_to_string("./data/input.txt")
-        .expect("Couldn't read the file");
-
-    let arr = contents.split("\n\n");
-    let mut elves = Vec::new();
-
-    for x in arr {
-        elves.push(calorie_sum(x));
-    }
-
-    elves.sort();
-    elves.reverse();
-
-    println!("{:?}", elves[0] + elves[1] + elves[2]);
+    let contents = include_str!("./data/input.txt").to_string();
+    let games: Vec<String> = contents.split("\n").map(|s| s.to_string()).collect();
+    let scores: Vec<u32> = games.iter().map(|game| game_to_points(game)).collect();
+    println!("scores: {:?}", scores.iter().sum::<u32>());
 }
 
-fn calorie_sum(calories: &str) -> i32 {
-    let cal_arr = calories.split("\n");
-    let mut total = 0;
-    for cal in cal_arr {
-        total += i32::from_str(cal).unwrap();
+fn game_to_points(game:&String) -> u32 {
+    let equiv = HashMap::from([
+        ("A", "X"),
+        ("B", "Y"),
+        ("C", "Z"),
+    ]);
+
+    let beats = HashMap::from([
+        ("A", "Z"),
+        ("B", "X"),
+        ("C", "Y"),
+    ]);
+
+    let home_value = HashMap::from([
+        ("X", 1),
+        ("Y", 2),
+        ("Z", 3),
+    ]);
+
+    let symbols: Vec<&str> = game.split(" ").collect();
+    let bonus = home_value[symbols[1]];
+
+    if beats[symbols[0]] == symbols[1] {
+        return 0 + bonus;
+    } 
+    else if equiv[symbols[0]] == symbols[1] {
+        return 3 + bonus;
     }
-    total
+    6 + bonus
+
 }

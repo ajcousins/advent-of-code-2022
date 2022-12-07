9bb6e615c26cc14d1742ef537a4e0d07e3bf118a
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-12-07 18:16:08 +0000

    Day 3.1

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import fs from 'fs';
+
+const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
+
+const value = (char: string): number =>
+  char.charCodeAt(0) < 97 ? char.charCodeAt(0) - 38 : char.charCodeAt(0) - 96;
+
+const halve = (line) => {
+  const midpoint = line.length / 2;
+  return [line.substring(0, midpoint), line.substring(midpoint)];
+};
+
+const partOne = (input: string[]) =>
+  input
+    .map((line) => halve(line))
+    .map((halves) => {
+      const [first, second] = halves;
+      let odd: string;
+      first.split('').forEach((char) => {
+        if (second.includes(char)) {
+          odd = char;
+        }
+      });
+      return value(odd);
+    })
+    .reduce((acc, val) => {
+      return acc + val;
+    }, 0);
+
+console.log("Part One:", partOne(input));

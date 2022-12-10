e0d857d7a43e5ad2a0114b07cdaa7b2e04e3ebb8
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-12-10 01:47:53 +0000

    Day 7.1

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,71 @@
 import fs from 'fs';
-const input = fs.readFileSync('input.txt', 'utf-8');
+const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
+class Folder {
+    constructor(name, parent) {
+        this.name = name;
+        this.parent = parent;
+        this.contents = [];
+        this.folderSize = 0;
+    }
+}
+class File {
+    constructor(name, filesize) {
+        this.name = name;
+        this.filesize = filesize;
+    }
+}
+const results = [];
+const folderSizes = (struct) => {
+    const structCopy = { ...struct };
+    let contentsSum = 0;
+    structCopy.contents.forEach((child, i) => {
+        if (child['folderSize'] === 0) {
+            const childFolder = folderSizes(child);
+            results.push({ name: childFolder.name, size: childFolder.folderSize });
+            contentsSum += childFolder.folderSize;
+        }
+        else {
+            contentsSum += child.filesize;
+        }
+    });
+    return { ...structCopy, folderSize: contentsSum };
+};
 const puzzle = (input) => {
-    const chars = input.split('');
+    const fileStruct = new Folder('/', null);
+    let curRef = fileStruct;
     let cursor = 0;
-    while (cursor < chars.length) {
-        const window = chars.slice(cursor, cursor + 14);
-        if (new Set(window).size === window.length)
-            break;
+    while (cursor < input.length) {
+        if (input[cursor] === '$ cd /') {
+            curRef = fileStruct;
+        }
+        else if (input[cursor] === '$ cd ..') {
+            curRef = curRef.parent;
+        }
+        else if (input[cursor].includes('$ cd ')) {
+            const targetFilename = input[cursor].slice(5);
+            const contents = curRef.contents;
+            const idx = contents.findIndex((item) => item.name === targetFilename);
+            if (idx > -1) {
+                curRef = curRef.contents[idx];
+            }
+        }
+        if (input[cursor].slice(0, 4) === 'dir ') {
+            const folderName = input[cursor].slice(4);
+            const newFolder = new Folder(folderName, curRef);
+            curRef.contents.push(newFolder);
+        }
+        if (!isNaN(+input[cursor].slice(0, 1))) {
+            const [size, name] = input[cursor].split(' ');
+            const newFile = new File(name, Number(size));
+            curRef.contents.push(newFile);
+        }
         cursor++;
     }
-    return cursor + 14;
+    folderSizes(fileStruct);
 };
-console.log(puzzle(input));
+puzzle(input);
+const answer = results
+    .filter((log) => log.size <= 100000)
+    .reduce((acc, val) => acc + val.size, 0);
+console.log('answer:', answer);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,98 @@
 import fs from 'fs';
 
-const input = fs.readFileSync('input.txt', 'utf-8');
+const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
 
-const puzzle = (input: string) => {
-  const chars = input.split('');
-  let cursor = 0;
-  while (cursor < chars.length) {
-    const window = chars.slice(cursor, cursor + 14);
-    if (new Set(window).size === window.length) break;
-    cursor++;
+interface Folder {
+  name: string;
+  parent: Folder;
+  contents: (Folder | File)[];
+  folderSize: number;
+}
+
+interface File {
+  name: string;
+  filesize: number;
+}
+
+class Folder {
+  constructor(name: string, parent: Folder | null) {
+    this.name = name;
+    this.parent = parent;
+    this.contents = [];
+    this.folderSize = 0;
   }
-  return cursor + 14;
+}
+
+class File {
+  constructor(name: string, filesize: number) {
+    this.name = name;
+    this.filesize = filesize;
+  }
+}
+interface Log {
+  name: string;
+  size: number;
+}
+
+const results: Log[] = [];
+
+const folderSizes = (struct: Folder): Folder => {
+  const structCopy = { ...struct };
+  let contentsSum = 0;
+  structCopy.contents.forEach((child, i) => {
+    if (child['folderSize'] === 0) {
+      const childFolder = folderSizes(child as Folder);
+      results.push({ name: childFolder.name, size: childFolder.folderSize });
+      contentsSum += childFolder.folderSize;
+    } else {
+      contentsSum += (child as File).filesize;
+    }
+  });
+  return { ...structCopy, folderSize: contentsSum };
 };
 
-console.log(puzzle(input));
+const puzzle = (input: string[]) => {
+  const fileStruct = new Folder('/', null);
+  let curRef: Folder = fileStruct;
+  let cursor = 0;
+  while (cursor < input.length) {
+    if (input[cursor] === '$ cd /') {
+      curRef = fileStruct;
+    } else if (input[cursor] === '$ cd ..') {
+      curRef = curRef.parent;
+    } else if (input[cursor].includes('$ cd ')) {
+      const targetFilename = input[cursor].slice(5);
+      const contents = curRef.contents;
+      const idx = contents.findIndex(
+        (item: File | Folder) => item.name === targetFilename
+      );
+
+      if (idx > -1) {
+        curRef = curRef.contents[idx] as Folder;
+      }
+    }
+
+    if (input[cursor].slice(0, 4) === 'dir ') {
+      const folderName = input[cursor].slice(4);
+      const newFolder = new Folder(folderName, curRef);
+      curRef.contents.push(newFolder);
+    }
+
+    if (!isNaN(+input[cursor].slice(0, 1))) {
+      const [size, name] = input[cursor].split(' ');
+      const newFile = new File(name, Number(size));
+      curRef.contents.push(newFile);
+    }
+
+    cursor++;
+  }
+  folderSizes(fileStruct);
+};
+
+puzzle(input);
+
+const answer = results
+  .filter((log) => log.size <= 100000)
+  .reduce((acc, val) => acc + val.size, 0);
+
+console.log('answer:', answer);

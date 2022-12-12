0c568001028886f92c9a6c53715a0f99d09e07b8
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-12-12 18:52:23 +0000

    Day 10.1

--- dist/index.js
+++ dist/index.js
@@ -1,77 +1,29 @@
 import fs from 'fs';
 const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
-const getFrames = (curHeadPos, dir, magnitude) => {
-    let pos = { ...curHeadPos };
-    let cmd = {
-        x: dir === 'R' ? 1 : dir === 'L' ? -1 : 0,
-        y: dir === 'U' ? 1 : dir === 'D' ? -1 : 0,
-    };
-    let repeat = magnitude;
-    const frames = [];
-    while (repeat > 0) {
-        pos = { x: pos.x + cmd.x, y: pos.y + cmd.y };
-        frames.push(pos);
-        repeat--;
-    }
-    return frames;
-};
-const knotFramesMap = (headFrames) => {
-    let curTailPos = { x: 0, y: 0 };
-    const frames = headFrames.map((frame, i) => {
-        if (i === 0)
-            return frame;
-        if (frame.x === curTailPos.x && frame.y === curTailPos.y) {
-            curTailPos = frame;
-            return frame;
-        }
-        const xDiff = frame.x - curTailPos.x;
-        const yDiff = frame.y - curTailPos.y;
-        if (Math.abs(xDiff) < 2 && Math.abs(yDiff) < 2) {
-            return curTailPos;
-        }
-        if ((Math.abs(xDiff) === 2 && Math.abs(yDiff) === 0) ||
-            (Math.abs(xDiff) === 0 && Math.abs(yDiff) === 2)) {
-            curTailPos = {
-                x: curTailPos.x + xDiff * 0.5,
-                y: curTailPos.y + yDiff * 0.5,
-            };
-            return curTailPos;
-        }
-        if (Math.abs(xDiff) >= 1 && Math.abs(yDiff) >= 1) {
-            curTailPos = {
-                x: curTailPos.x + (Math.abs(xDiff) > 1 ? xDiff * 0.5 : xDiff),
-                y: curTailPos.y + (Math.abs(yDiff) > 1 ? yDiff * 0.5 : yDiff),
-            };
-            return curTailPos;
-        }
-    });
-    return frames;
-};
 const puzzle = (input) => {
-    let curHeadPos = { x: 0, y: 0 };
-    let headFrames = [curHeadPos];
-    input.forEach((cmd) => {
-        const [dir, magnitude] = cmd.split(' ');
-        const positions = getFrames(curHeadPos, dir, Number(magnitude));
-        curHeadPos = positions[positions.length - 1];
-        headFrames = headFrames.concat(positions);
-    });
-    const knots = Array.apply(null, Array(9)).map((x, i) => i);
-    let frames = headFrames;
-    knots.forEach(k => {
-        frames = knotFramesMap(frames);
-    });
-    const hash = {};
-    frames.forEach((frame) => {
-        const idxString = JSON.stringify(frame);
-        if (!hash[idxString]) {
-            hash[idxString] = 1;
+    let X = 1;
+    const V = [0];
+    const record = [];
+    const cyclesOfInterest = [20, 60, 100, 140, 180, 220];
+    let cycleNum = 1;
+    while (cycleNum < 221) {
+        const line = input[cycleNum - 1];
+        const signalStrength = cycleNum * X;
+        if (cyclesOfInterest.includes(cycleNum)) {
+            record.push({ cycleNum, signalStrength });
+        }
+        if (line === 'noop') {
+            V.push(0);
         }
         else {
-            hash[idxString] += 1;
+            const val = line?.split(' ')[1] ?? 0;
+            V.push(Number(val));
+            V.push(0);
         }
-    });
-    return Object.keys(hash).length;
+        X += V.shift();
+        cycleNum++;
+    }
+    return record.reduce((acc, val) => acc + val.signalStrength, 0);
 };
 console.log(puzzle(input));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,95 +2,30 @@ import fs from 'fs';
 
 const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
 
-interface Coord {
-  x: number;
-  y: number;
-}
-
-interface Hashmap {
-  [index: string]: number;
-}
-
-const getFrames = (
-  curHeadPos: Coord,
-  dir: string,
-  magnitude: number
-): Coord[] => {
-  let pos = { ...curHeadPos };
-  let cmd = {
-    x: dir === 'R' ? 1 : dir === 'L' ? -1 : 0,
-    y: dir === 'U' ? 1 : dir === 'D' ? -1 : 0,
-  };
-  let repeat = magnitude;
-  const frames = [];
-  while (repeat > 0) {
-    pos = { x: pos.x + cmd.x, y: pos.y + cmd.y };
-    frames.push(pos);
-    repeat--;
-  }
-  return frames;
-};
-
-const knotFramesMap = (headFrames: Coord[]): Coord[] => {
-  let curTailPos = { x: 0, y: 0 };
-  const frames = headFrames.map((frame, i): Coord => {
-    if (i === 0) return frame;
-    if (frame.x === curTailPos.x && frame.y === curTailPos.y) {
-      curTailPos = frame;
-      return frame;
-    }
-    const xDiff = frame.x - curTailPos.x;
-    const yDiff = frame.y - curTailPos.y;
-    if (Math.abs(xDiff) < 2 && Math.abs(yDiff) < 2) {
-      return curTailPos;
-    }
-    if (
-      (Math.abs(xDiff) === 2 && Math.abs(yDiff) === 0) ||
-      (Math.abs(xDiff) === 0 && Math.abs(yDiff) === 2)
-    ) {
-      curTailPos = {
-        x: curTailPos.x + xDiff * 0.5,
-        y: curTailPos.y + yDiff * 0.5,
-      };
-      return curTailPos;
-    }
-    if (Math.abs(xDiff) >= 1 && Math.abs(yDiff) >= 1) {
-      curTailPos = {
-        x: curTailPos.x + (Math.abs(xDiff) > 1 ? xDiff * 0.5 : xDiff),
-        y: curTailPos.y + (Math.abs(yDiff) > 1 ? yDiff * 0.5 : yDiff),
-      };
-      return curTailPos;
-    }
-  });
-  return frames;
-};
-
 const puzzle = (input: string[]) => {
-  let curHeadPos = { x: 0, y: 0 };
-  let headFrames: Coord[] = [curHeadPos];
-  input.forEach((cmd) => {
-    const [dir, magnitude] = cmd.split(' ');
-    const positions = getFrames(curHeadPos, dir, Number(magnitude));
-    curHeadPos = positions[positions.length - 1];
-    headFrames = headFrames.concat(positions);
-  });
-  const knots = Array.apply(null, Array(9)).map((x, i) => i);
-  let frames = headFrames;
-  knots.forEach((k) => {
-    frames = knotFramesMap(frames);
-  });
-
-  const hash: Hashmap = {};
-  frames.forEach((frame) => {
-    const idxString = JSON.stringify(frame);
-    if (!hash[idxString]) {
-      hash[idxString] = 1;
-    } else {
-      hash[idxString] += 1;
+  let X = 1;
+  const V = [0];
+  const record = [];
+  const cyclesOfInterest = [20, 60, 100, 140, 180, 220];
+  let cycleNum = 1;
+  while (cycleNum < 221) {
+    const line = input[cycleNum - 1];
+    const signalStrength = cycleNum * X;
+    if (cyclesOfInterest.includes(cycleNum)) {
+      record.push({ cycleNum, signalStrength });
     }
-  });
+    if (line === 'noop') {
+      V.push(0);
+    } else {
+      const val = line?.split(' ')[1] ?? 0;
+      V.push(Number(val));
+      V.push(0);
+    }
+    X += V.shift();
+    cycleNum++;
+  }
 
-  return Object.keys(hash).length;
+  return record.reduce((acc, val) => acc + val.signalStrength, 0);
 };
 
 console.log(puzzle(input));
